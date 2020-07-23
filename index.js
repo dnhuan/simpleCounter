ec32a6aef44bf68974beb19a989afb089ae89e05
dnhuan <huandngoc@gmail.com>
2020-07-24 00:49:31 +0700

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const httpServer = require("http").createServer(app);
+const io = require("socket.io")(httpServer);
+const bodyParser = require("body-parser")
+const cors = require("cors");
+
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
+const adapter = new FileSync('db.json');
+const db = low(adapter)
+
+db.defaults({ count: 0 })
+  .write()
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req,res)=>{
+    res.render(__dirname + "/index", {count: getCount()});
+})
+
+app.get('/count', (req,res)=>{
+    res.send(getCount())
+})
+
+app.all('/update', (req,res)=>{
+    let count = parseInt(db.get('count').value()) + 1
+    db.set('count', count).write();
+    io.emit('count', count);
+    res.sendStatus(200);
+})
+
+app.all('/reset', (req,res)=>{
+    db.set('count', 0).write();
+    res.sendStatus(200)
+})
+
+setInterval(()=>{
+    io.emit('count', getCount())
+}, 1000)
+
+httpServer.listen(process.env.PORT || 3000, () => {
+    console.log("Listening on port", process.env.PORT || 3000);
+});
+
+function getCount(){
+    return db.get('count').value().toString();
+}
